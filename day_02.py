9747500813baab720a4b58019a431e6b0b6a658c
rickdkk <r.de.klerk@pl.hanze.nl>
2022-12-02 21:04:15 +0100

    Day 2

---
+++ day_02.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+ENCODING = {"A": "Rock", "B": "Paper", "C": "Scissors", "X": "Rock", "Y": "Paper", "Z": "Scissors"}
+
+ENCODING_GAME2 = {"X": {"Rock": "Scissors", "Paper": "Rock", "Scissors": "Paper"},  # player has to lose
+                  "Y": {"Rock": "Rock", "Paper": "Paper", "Scissors": "Scissors"},  # draw, maps elf to player move
+                  "Z": {"Rock": "Paper", "Paper": "Scissors", "Scissors": "Rock"}}  # win
+
+GAME = {"Rock": {"Rock": 4, "Paper": 1, "Scissors": 7},  # Player plays rock, opponent plays rock/paper/scissors
+        "Paper": {"Rock": 8, "Paper": 5, "Scissors": 2},  # Win is 6 points, draw is 3 points, lose is 0 points
+        "Scissors": {"Rock": 3, "Paper": 9, "Scissors": 6}}  # Rock is 1 point, paper is 2 points, scissors is 3 points
+
+# Read and parse data
+games = [move.split(" ") for move in Path("data/day_02.txt").read_text().split("\n")]  # ["C X", "C Y", "B X", ...]
+
+# Game 1
+sum([GAME[ENCODING[player]][ENCODING[elf]] for elf, player in games])
+
+# Game 2
+sum([GAME[ENCODING_GAME2[player][ENCODING[elf]]][ENCODING[elf]] for elf, player in games])
