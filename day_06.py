8f9615576341cd1a0f9c27925c27b20b1a4e861b
rickdkk <r.de.klerk@pl.hanze.nl>
2022-12-06 19:18:16 +0100

    Day 6

---
+++ day_06.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+
+# %% Read data
+characters = Path("data/day_06.txt").read_text()
+
+# %% Question 1 and 2: Find first series of four and fourteen unique characters
+for key_length in (4, 14):
+    current_items = []
+    for idx, character in enumerate(characters):
+        if character in current_items:
+            current_items = current_items[current_items.index(character) + 1:]  # 'reset' from the duplicate onward
+
+        current_items.append(character)
+
+        if len(current_items) == key_length:
+            print(idx + 1)
+            break
